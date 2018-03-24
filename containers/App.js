be3a30a950498dae20d54c6bc28ccffd763dbb66
Benjamin Chirlin <info@benjaminchirlin.com>
2018-03-24 02:31:59 -0700

    Attempting to get babel webpack up

---
+++ containers/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class App extends Component {
+  render () {
+    return
+      <p>Excuse my mess but I decided to redo my website while I look for a new work.</p>
+      <p>In my glee I attempted to update some dependencies on here.</p>
+      <p>This in turn led to some rash decisions which resulted in breaking many things.</p>
+      <p>Long story short: under construction!</p>
+  }
+}
