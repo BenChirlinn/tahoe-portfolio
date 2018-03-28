1e9da6450decf89accc91fad8798dfff69c5a566
Ben Chirlin <info@benjaminchirlin.com>
2018-03-28 01:37:18 -0700

    Cleaning up texture styling
    Do everything with pseudo-elements so we don't need spans that just
    exist to add texture.

--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 const Header = ({title}) => {
   return (
-    <div id="page-header">
-      <span className="texture sunset-texture"></span>
-      <span className="sun"><span className="texture sun-texture"></span></span>
+    <div id="page-header" className="sunset-texture">
+      <span className="sun sun-texture"></span>
       <h1 dangerouslySetInnerHTML={{__html: title}}></h1>
       <span className="mountains"></span>
     </div>
