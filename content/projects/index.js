1c9f34d73be28b61b7904f9c96c3770b8e70d9be
Ben Chirlin <info@benjaminchirlin.com>
2018-04-05 02:39:01 -0700

    Roughing out all project content
    Grouping and restructuring some stuff for cleanliness and emphasis.

--- content/projects/index.js
+++ content/projects/index.js
@@ -1,6 +1,31 @@
 const projectData = {
-  nosey: require('./nosey.json'),
-  'paint-portfolio': require('./paint-portfolio.json')
+  // Professional digital/web projects
+  // # WIRED
+  'wired': require('./wired.json'),
+  'wired-pangea': require('./wired-pangea.json'),
+  'wired-overhaul': require('./wired-overhaul.json'),
+  // # The Mechanism
+  'the-mechanism': require('./the-mechanism.json'),
+  'james-beard-foundation': require('./james-beard-foundation.json'),
+  'financial-access-iniative': require('./financial-access-iniative.json'),
+  'my-morning-jacket': require('./my-morning-jacket.json'),
+  // # Personal
+  'yodlr': require('./yodlr.json'),
+  'paint-portfolio': require('./paint-portfolio.json'),
+  // # Other
+  'mindsnacks': require('./mindsnacks.json'),
+  'design-work': require('./design-work.json'),
+  'nightime-adventure-society': require('./nightime-adventure-society.json'),
+  'ultimate-jerseys': require('./ultimate-jerseys.json'),
+  'my-artwork': require('./nosey.json'),
+  // # College
+  'nosey-website': require('./nosey-website.json'),
+  'programmers-best-friend': require('./programmers-best-friend.json'),
+  'college-web-design': require('./college-web-design.json'),
+  'digital-design-foundations': require('./digital-design-foundations.json'),
+  'nosey': require('./nosey.json'),
+  'chosen-sight': require('./chosen-sight.json'),
+  'sequential-art': require('./sequential-art.json')
 }
 
 export default projectData;
