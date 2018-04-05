78a1abac130cd83ba7784e420f389b2391885b96
Ben Chirlin <info@benjaminchirlin.com>
2018-04-05 02:06:18 -0700

    Storing data in separate JSON
    Keeping things clean.

--- containers/App.js
+++ containers/App.js
@@ -1,61 +1,23 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import _ from 'lodash';
+import projectData from '../content/projects';
 
 import HomepageContainer from './HomepageContainer';
 import ProjectContainer from './ProjectContainer';
 
 class App extends Component {
   componentWillMount() {
-    const projects = {
-      nosey: {
-        name: 'nosey',
-        title: 'Nosey',
-        body: 'A website for my animation',
-        gallery: [
-          {
-            img: 'an-img.jpg',
-            caption: 'A img 1 caption'
-          },
-          {
-            img: 'an-img.jpg',
-            caption: 'A img 2 caption'
-          },
-          {
-            img: 'an-img.jpg',
-            caption: 'A img 3 caption'
-          }
-        ]
-      },
-      'paint-portfolio': {
-        name: 'paint-portfolio',
-        title: 'Paint Portfolio Site',
-        body: 'A Wordpress website for my portfolio',
-        gallery: [
-          {
-            img: 'an-img.jpg',
-            caption: 'A img 1 caption'
-          },
-          {
-            img: 'an-img.jpg',
-            caption: 'A img 2 caption'
-          },
-          {
-            img: 'an-img.jpg',
-            caption: 'A img 3 caption'
-          }
-        ]
-      }
-    };
-    this.setState({ projects });
+    this.setState({ projectData });
   }
 
   render() {
     console.log(this.state);
-    const { projects = {} } = this.state;
-    // console.log(projects);
-    // console.log(_.find(projects, p => p.name === 'nosey'));
+    const { projectData = {} } = this.state;
+    // console.log(projectData);
+    // console.log(_.find(projectData, p => p.name === 'nosey'));
     // @todo repalce with separate var when no longer under construction
     if (this.props.env === 'production') {
       return <underConstructionContainer />;
@@ -64,9 +26,9 @@ class App extends Component {
         <Router>
           <div>
             <Route exact path="/" component={HomepageContainer} />
-            {projects && (
+            {projectData && (
               <Route path="/project/:name" render={({ match }) => (
-                <ProjectContainer project={ _.find(projects, p => p.name === match.params.name) } />
+                <ProjectContainer project={ _.find(projectData, p => p.name === match.params.name) } />
               )} />
           )}
           </div>
---
+++ content/projects/index.js
@@ -0,0 +1,6 @@
+const projectData = {
+  nosey: require('./nosey.json'),
+  'paint-portfolio': require('./paint-portfolio.json')
+}
+
+export default projectData;
