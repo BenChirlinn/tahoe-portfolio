d909d0357bbae3db9704988efd3cee2407029ebe
Ben Chirlin <info@benjaminchirlin.com>
2018-04-26 15:34:58 -0700

    Fixing up spacing and adding email

--- components/HomepageBody.js
+++ components/HomepageBody.js
@@ -7,7 +7,7 @@ const HomepageBody = () => (
   <div id='homepage-component'>
     <div className='homepage-section'>
       <p>Welcome. I am a fullstack web developer in the Bay Area. Nothing makes me happier than beautiful site with beautiful code beneath the surface.</p>
-      <p>Currently I’m looking for my next big move.</p>
+      <p>Currently I’m looking for my next big move. If you know of something drop me a line at <a title='Email me' href='mailto:info@benjaminchirlin.com' target='_blank'>info@benjaminchirlin.com</a>.</p>
       <p>You can find me elsewhere around the web at {
         socialLinks.map((platform, index, socialLinks) => <span key={index}>{index === socialLinks.length - 1 ? 'and ' : '' }<SocialLink title={platform.title} url={platform.url} icon={false} />{index <socialLinks.length - 1 ? ', ' : ''}</span>)
       }.</p>
