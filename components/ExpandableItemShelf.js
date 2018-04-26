6ce717f3aa477f2f72972e2ebbe86883410a94b7
Ben Chirlin <info@benjaminchirlin.com>
2018-04-26 02:04:11 -0700

    Adding skillls and links to projects

--- components/ExpandableItemShelf.js
+++ components/ExpandableItemShelf.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
-const ExpandableItemShelf = ({ item, itemType }) => (
-  <div className='list-item-shelf-wrapper'>
-    <div className='list-item-synopsis' dangerouslySetInnerHTML={{__html: item.synopsis}} />
-  </div>
-);
+import _ from 'lodash';
+
+const ExpandableItemShelf = ({ item, itemType }) => {
+  let extraContent = '';
+  if (itemType === 'project') {
+    if (!_.isEmpty(item.skills)) {
+      const skillList = item.skills.reduce((skillString, skill, index, skills) => (index < skills.length - 1 ? `${skillString}${skill}, ` : `${skillString}${skill}`), '');
+      console.log(skillList);
+      extraContent += `<p>Skills: ${skillList}</p>`;
+    }
+
+    if (_.get(item, 'link', false)) {
+      extraContent += `<p><a href='${item.link}' title='${item.title}' target='_blank'>Visit Project</a></p>`;
+    }
+  }
+
+  return <div className='list-item-shelf-wrapper'>
+    <div className='list-item-synopsis' dangerouslySetInnerHTML={{__html: item.synopsis + extraContent}} />
+    </div>
+};
 
 export default ExpandableItemShelf;
