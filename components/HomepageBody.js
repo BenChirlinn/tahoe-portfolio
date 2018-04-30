ccddaaa58a21939cc4cf78f07b7bf4785a1ddafc
Ben Chirlin <info@benjaminchirlin.com>
2018-04-30 14:12:20 -0700

    Updating content to mirror resume a bit more.

--- components/HomepageBody.js
+++ components/HomepageBody.js
@@ -6,7 +6,7 @@ import SocialLink from './SocialLink';
 const HomepageBody = () => (
   <div id='homepage-component'>
     <div className='homepage-section'>
-      <p>Welcome. I am a fullstack web developer in the Bay Area. Nothing makes me happier than beautiful site with beautiful code beneath the surface.</p>
+      <p>Welcome. I am a full stack engineer in the Bay Area. Nothing makes me happier than a beautiful site with gorgeous code beneath the surface.</p>
       <p>Currently I’m looking for my next big move. If you know of something drop me a line at <a title='Email me' href='mailto:info@benjaminchirlin.com' target='_blank'>info@benjaminchirlin.com</a>.</p>
       <p>You can find me elsewhere around the web at {
         socialLinks.map((platform, index, socialLinks) => <span key={index}>{index === socialLinks.length - 1 ? 'and ' : '' }<SocialLink title={platform.title} url={platform.url} icon={false} />{index <socialLinks.length - 1 ? ', ' : ''}</span>)
